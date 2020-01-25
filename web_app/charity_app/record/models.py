689d4dfc864cd46842f3720dbd464695a79570f3
Charlotte Bermingham <cbermingham@hotmail.co.uk>
2020-01-25 14:55:39 +0000

    register record model with admin site

--- web_app/charity_app/record/models.py
+++ web_app/charity_app/record/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Record(models.Model):
-	charity = models.CharField('Event Name', max_length=120)
+	charity = models.CharField('Charity', max_length=120)
 	item = models.CharField(max_length=120)
 	time = models.CharField(max_length=120)
 	location = models.CharField(max_length = 60)
