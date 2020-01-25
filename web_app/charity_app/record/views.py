c45ab6cb8733432d712e53755619fd3ca27d474e
Charlotte Bermingham <cbermingham@hotmail.co.uk>
2020-01-25 14:41:18 +0000

    create template

--- web_app/charity_app/record/views.py
+++ web_app/charity_app/record/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("<h1>Streetwise</h1>")
+	title = "Streetwise"
+	#return HttpResponse(title)
+	return render(request, 'base.html', {'title': title})
+
 
 # Create your views here.
