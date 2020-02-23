c08fd61cbe4fb89c44a80278c355804367f95314
Kamran Soomro <kamran.soomro@uwe.ac.uk>
2020-02-23 23:00:54 +0000

    Added responsive menu and geolocation retrieval.

--- web_app/charity_app/record/forms.py
+++ web_app/charity_app/record/forms.py
@@ -2,18 +2,23 @@ from django import forms
 from django.forms import ModelForm
 from .models import Record, Item
 
+
 # create the form class for the Record model
 class RecordForm(ModelForm):
-	item = forms.ModelChoiceField(
-		queryset=Item.objects.all(),
-		empty_label="Please select an item..."
-	)
-	class Meta:
-		model = Record
-		fields = '__all__'
-		# fields = ['charity', 'time', 'location']
-		# widgets = {
-			# 'charity': forms.TextInput(
-			# 	attrs={'placeholder': 'Charity'}
-			# )
-		# }
+    item = forms.ModelChoiceField(
+        queryset=Item.objects.all(),
+        empty_label="Please select an item..."
+    )
+
+    class Meta:
+        model = Record
+        fields = '__all__'
+        # fields = ['charity', 'time', 'location']
+        widgets = {
+            'location': forms.TextInput(
+                attrs={
+                    'readonly': 'true',
+                    'placeholder': 'Retrieving...'
+                }
+            )
+        }
