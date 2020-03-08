b2078c53b72bf8216de047e2f526628d36856f60
Charlotte Bermingham <cbermingham@hotmail.co.uk>
2020-03-08 20:27:52 +0000

    Make dashboard containing plot

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,11 @@
-import plotly.offline as pyo
 import plotly.graph_objs as go
 import sqlite3
 import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+
 
 # connect to the database and make a query to retrieve the number of rows for each charity
 conn = sqlite3.connect('/Users/Charlotte/Documents/Code/Bristol_homeless_hackathon/web_app/charity_app/db.sqlite3')
@@ -16,11 +20,27 @@ query = '''SELECT Charity, COUNT(1) AS Interactions
 df = pd.read_sql_query(query, conn)
 df.set_index('charity', inplace=True)
 
+# Launch the application:
+app = dash.Dash()
+
 # set the data and layout variables then plot a bar plot of the number of interactions for each charity.
 
 data = go.Bar(x=df.index, y=df['Interactions'])
 layout = go.Layout(title = 'Charity Interactions', yaxis_title='Interactions', xaxis_title='Charity')
-fig = go.Figure(data=data, layout=layout)
 
-# save the html file for the figure
-pyo.plot(fig, filename='charity_interactions_bar_chart.html')
+# Create a Dash layout that contains a Graph component:
+
+app.layout = html.Div(children=[
+	html.H1(children='Charity Activity'),
+    html.Div(children='Adjust the plots to explore the activity of homeless charities in Bristol'),
+	dcc.Graph(id='charity_bar',
+                figure = {'data':[data],
+                            'layout':layout
+                            }
+                            
+		)])
+
+# Add the server clause:
+
+if __name__ == '__main__':
+    app.run_server()
