fca6e6ee2f96c9b1424ac81a4c65739dd8204218
Charlotte Bermingham <cbermingham@hotmail.co.uk>
2020-03-15 13:09:14 +0000

    Interactive dashboard with multiple dropdown to select charities on bar plot - working

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,7 @@ import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 
 
@@ -20,25 +21,41 @@ query = '''SELECT Charity, COUNT(1) AS Interactions
 df = pd.read_sql_query(query, conn)
 df.set_index('charity', inplace=True)
 
+# create a dictionary for the dropdown containing all the charity names
+charity_options = []
+for i in df.index:
+    charity_options.append({'label':i,'value':i})
+
 # Launch the application:
 app = dash.Dash()
 
-# set the data and layout variables then plot a bar plot of the number of interactions for each charity.
 
-data = go.Bar(x=df.index, y=df['Interactions'])
-layout = go.Layout(title = 'Charity Interactions', yaxis_title='Interactions', xaxis_title='Charity')
+# Create a Dash layout that contains a dropdown with multiple items and a Graph component:
+app.layout = html.Div([
+    dcc.Dropdown(id='charity-input',options=charity_options,value=['Soup Run Trust'], multi = True),
+    dcc.Graph(id='graph')
+])
 
-# Create a Dash layout that contains a Graph component:
 
-app.layout = html.Div(children=[
-	html.H1(children='Charity Activity'),
-    html.Div(children='Adjust the plots to explore the activity of homeless charities in Bristol'),
-	dcc.Graph(id='charity_bar',
-                figure = {'data':[data],
-                            'layout':layout
-                            }
-                            
-		)])
+# Call back to take in the values from the dropdown and use to update the graph
+@app.callback(Output('graph', 'figure'),
+              [Input('charity-input', 'value')])
+def update_figure(selected_charities):
+    # x data is the charities selected in the dropdown, y data is the corresponding number of interactions 
+    # for these charities
+    x_data = selected_charities
+    y_data = df.loc[selected_charities]['Interactions']
+    # make the data list for a bar plot using the x and y data and set the layout parameters.
+    # These are returned to update the bar plot
+    return {
+        'data': [go.Bar(x=x_data, y=y_data)],
+        'layout': go.Layout(
+            xaxis={'title': 'Charity'},
+            yaxis={'title': 'Number of Interactions'},
+            hovermode='closest'
+        )
+    }
+
 
 # Add the server clause:
 
